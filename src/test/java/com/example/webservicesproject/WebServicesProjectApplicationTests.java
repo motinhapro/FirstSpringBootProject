3daf49bb097e9592c56dac7bf5241b94dc3a5b9c
Carlos Eduardo <carloseduardomotaed@gmail.com>
2025-07-01 15:59:05 -0300

    First commit

---
+++ src/test/java/com/example/webservicesproject/WebServicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
