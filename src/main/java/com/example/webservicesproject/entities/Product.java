e4f8e22de7fd81ea64e0e20b9d96f30448ff5fd4
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-07-04 17:53:30 -0300

    feat: add Product-OrderItem one-to-many association

--- src/main/java/com/example/webservicesproject/entities/Product.java
+++ src/main/java/com/example/webservicesproject/entities/Product.java
@@ -1,5 +1,6 @@
 package com.example.webservicesproject.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,12 +34,27 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product(Long id, String name, String description,  double price, String imgUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.description = description;
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+
+        for(OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+
+        return set;
     }
 
     @Override
